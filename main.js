2a29b6b05790869b208472b839f63c3139f7f1a9
carlosemartins97 <carlosemartins97@gmail.com>
2020-07-06 08:01:57 -0300

    adicionando a lógica

--- main.js
+++ main.js
@@ -1,14 +1,27 @@
 var cores = [
-    "rgb(255, 0, 0)"
+    "rgb(255, 0, 0)",
     "rgb(255, 255, 0)",
     "rgb(0, 255, 0)",
     "rgb(0, 255, 255)",
     "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)",
+    "rgb(255, 0, 255)"
 ]
 
 var quadrados = document.querySelectorAll(".quadrado");
+var corCerta = cores[3];
+var corDisplay = document.getElementById("corDisplay");
 
-for(var i = 0; i > quadrados.length; i++) {
+corDisplay.textContent = corCerta;
+
+for(var i = 0; i < quadrados.length; i++){
     quadrados[i].style.backgroundColor = cores[i];
+
+    quadrados[i].addEventListener("click", function(){
+        var corClicada = this.style.backgroundColor;
+        if(corClicada === corCerta){
+            alert("correto");
+        } else {
+            alert("Errado!")
+        }
+    });
 }
