9054a9026c21b500e0046a2f71302d3543399594
carlosemartins97 <carlosemartins97@gmail.com>
2020-07-06 23:21:46 -0300

    Implementado dois níveis de dificuldade

--- main.js
+++ main.js
@@ -1,17 +1,52 @@
-var cores = escolherCorAleatoria(6);
+var numCores = 6;
+var cores = escolherCorAleatoria(numCores);
 var quadrados = document.querySelectorAll(".quadrado");
 var corCerta = pegarCor();
 var corDisplay = document.getElementById("corDisplay");
 var msg = document.querySelector("#msg");
 var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
+var easyButton = document.querySelector("#easy");
+var hardButton = document.querySelector("#hard");
 
-resetButton.addEventListener("click", function(){
-    cores = escolherCorAleatoria(6);
+
+easyButton.addEventListener("click", function(){
+    easyButton.classList.add("selected");
+    hardButton.classList.remove("selected");
+    numCores = 3;
+    cores = escolherCorAleatoria(numCores);
+    corCerta = pegarCor();
+    corDisplay.textContent = corCerta;
+    for(var i = 0; i < quadrados.length; i++){
+        if(cores[i]){
+            quadrados[i].style.backgroundColor = cores[i];
+        } else {
+            quadrados[i].style.display = "none";
+        }
+    }
+});
+
+hardButton.addEventListener("click", function(){
+    easyButton.classList.remove("selected");
+    hardButton.classList.add("selected");
+    numCores = 6;
+    cores = escolherCorAleatoria(numCores);
     corCerta = pegarCor();
     corDisplay.textContent = corCerta;
     for(var i = 0; i < quadrados.length; i++){
         quadrados[i].style.backgroundColor = cores[i];
+        quadrados[i].style.display = "block";
+    }
+});
+
+resetButton.addEventListener("click", function(){
+    cores = escolherCorAleatoria(numCores);
+    corCerta = pegarCor();
+    corDisplay.textContent = corCerta;
+    resetButton.textContent = "Novas cores";
+    for(var i = 0; i < quadrados.length; i++){
+        quadrados[i].style.backgroundColor = cores[i];
+        h1.style.backgroundColor = "#232323";
     }
 });
 
@@ -24,7 +59,8 @@ for(var i = 0; i < quadrados.length; i++){
         var corClicada = this.style.backgroundColor;
         console.log(corClicada, corCerta);
         if(corClicada === corCerta){
-            msg.textContent = "Play again?"
+            msg.textContent = "Correto!"
+            resetButton.textContent = "Jogar novamente?";
             mudarCores(corCerta);
             h1.style.backgroundColor = corCerta;
         } else {
